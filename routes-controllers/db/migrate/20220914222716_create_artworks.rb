925092345b7e9982bc89f556ebae51f39dfa67d5
AA Student <89243713+jason330@users.noreply.github.com>
2022-09-14 16:03:51 -0700

    Create artworks table

---
+++ routes-controllers/db/migrate/20220914222716_create_artworks.rb
@@ -0,0 +1,14 @@
+class CreateArtworks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :artworks do |t|
+      t.string :title, null: false
+      t.string :image_url, null: false, index: { unique: true }
+      t.bigint :artist_id, null: false
+      
+      t.index [:artist_id, :title], unique: true
+      t.timestamps
+
+    end
+
+  end
+end
